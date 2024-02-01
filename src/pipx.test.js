f3d92b497ec6a2efdfc66180b2928d411820c04a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-01 14:18:39 +0700

    test: modify `pipx.test.js` to use module mocking (#18)

--- src/pipx.test.js
+++ src/pipx.test.js
@@ -1,20 +1,43 @@
-import { exec } from "@actions/exec";
-import { pipxInstall } from "./pipx.mjs";
+import { jest } from "@jest/globals";
+
+let installedPkgs = [];
+
+jest.unstable_mockModule("@actions/exec", () => ({
+  exec: async (commandLine, args) => {
+    expect(commandLine).toBe("pipx");
+    expect(args.length).toBe(2);
+    expect(args[0]).toBe("install");
+
+    switch (args[1]) {
+      case "black":
+      case "ruff":
+        installedPkgs.push(args[1]);
+        break;
+
+      default:
+        throw new Error(`unknown package ${args[1]}`);
+    }
+  },
+}));
 
 describe("install Python packages", () => {
-  beforeAll(async () => {
-    await Promise.all([
-      exec("pipx", ["install", "black"]),
-      exec("pipx", ["install", "ruff"]),
-    ]);
+  beforeEach(() => {
+    installedPkgs = [];
   });
 
   it("should successfully install packages", async () => {
+    const { pipxInstall } = await import("./pipx.mjs");
+
     const prom = pipxInstall("black", "ruff");
     await expect(prom).resolves.toBeUndefined();
+
+    expect(installedPkgs).toContain("black");
+    expect(installedPkgs).toContain("ruff");
   });
 
   it("should fail to install an invalid package", async () => {
+    const { pipxInstall } = await import("./pipx.mjs");
+
     const prom = pipxInstall("invalid-pkg");
     await expect(prom).rejects.toThrow();
   });
