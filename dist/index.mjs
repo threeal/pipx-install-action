ed89d45e29cf1bfcc1cc6bc77a43f3570a29c1b9
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-01 15:35:15 +0700

    feat: install packages sequentially in `pipxInstall` function (#25)

--- dist/index.mjs
+++ dist/index.mjs
@@ -27244,7 +27244,9 @@ var exec = __nccwpck_require__(8434);
 ;// CONCATENATED MODULE: ./src/pipx.mjs
 
 async function pipxInstall(...pkgs) {
-    await Promise.all(pkgs.map((pkg) => (0,exec.exec)("pipx", ["install", pkg])));
+    for (const pkg of pkgs) {
+        await (0,exec.exec)("pipx", ["install", pkg]);
+    }
 }
 
 ;// CONCATENATED MODULE: ./src/main.mjs
