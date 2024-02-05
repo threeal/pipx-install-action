47d9617d679f215b34b36759a4ce9d3c5767baea
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-02-05 15:47:01 +0700

    feat: add cache support (#38)
    * feat: save package cache in `pipxInstallAction` function
    
    * feat: restore package cache in `pipxInstallAction` function if found

--- src/action.mts
+++ src/action.mts
@@ -3,8 +3,24 @@ import pipx from "./pipx/index.mjs";
 
 export async function pipxInstallAction(...pkgs: string[]): Promise<void> {
   for (const pkg of pkgs) {
-    await core.group(`Installing \u001b[34m${pkg}\u001b[39m...`, async () => {
-      await pipx.installPackage(pkg);
-    });
+    const cacheFound = await core.group(
+      `Restoring \u001b[34m${pkg}\u001b[39m cache...`,
+      async () => {
+        return pipx.restorePackageCache(pkg);
+      },
+    );
+
+    if (!cacheFound) {
+      await core.group(`Installing \u001b[34m${pkg}\u001b[39m...`, async () => {
+        await pipx.installPackage(pkg);
+      });
+
+      await core.group(
+        `Saving \u001b[34m${pkg}\u001b[39m cache...`,
+        async () => {
+          await pipx.savePackageCache(pkg);
+        },
+      );
+    }
   }
 }
--- src/action.test.js
+++ src/action.test.js
@@ -1,25 +1,59 @@
 import { jest } from "@jest/globals";
 
 let installedPkgs = [];
+let savedPkgsCaches = [];
 
-jest.unstable_mockModule("./pipx/install.mjs", () => ({
-  installPackage: async (pkg) => {
-    installedPkgs.push(pkg);
+jest.unstable_mockModule("./pipx/index.mjs", () => ({
+  default: {
+    installPackage: async (pkg) => {
+      switch (pkg) {
+        case "black":
+        case "ruff":
+          installedPkgs.push(pkg);
+          break;
+
+        default:
+          throw new Error("unknown package");
+      }
+    },
+    restorePackageCache: async (pkg) => {
+      if (savedPkgsCaches.includes(pkg)) {
+        installedPkgs.push(pkg);
+        return true;
+      }
+      return false;
+    },
+    savePackageCache: async (pkg) => {
+      savedPkgsCaches.push(pkg);
+    },
   },
 }));
 
 describe("install Python packages action", () => {
   beforeEach(() => {
     installedPkgs = [];
+    savedPkgsCaches = ["flake8"];
   });
 
-  it("should successfully install packages", async () => {
+  it("should successfully install and save packages", async () => {
     const { pipxInstallAction } = await import("./action.mjs");
 
     const prom = pipxInstallAction("black", "ruff");
     await expect(prom).resolves.toBeUndefined();
 
     expect(installedPkgs).toContain("black");
+    expect(savedPkgsCaches).toContain("black");
+
     expect(installedPkgs).toContain("ruff");
+    expect(savedPkgsCaches).toContain("ruff");
+  });
+
+  it("should successfully restore a package", async () => {
+    const { pipxInstallAction } = await import("./action.mjs");
+
+    const prom = pipxInstallAction("flake8");
+    await expect(prom).resolves.toBeUndefined();
+
+    expect(installedPkgs).toContain("flake8");
   });
 });
