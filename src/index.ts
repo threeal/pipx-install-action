3b6db26018cb74ebdff7ae4573f8ef2b0c98f6bb
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-05 15:41:54 +0700

    refactor: separate library to `pipx-install-action` workspace

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export { pipxInstallAction } from "./action.js";
+import * as core from "@actions/core";
+import { pipxInstallAction } from "pipx-install-action";
+
+async function main() {
+  const pkgs = core
+    .getInput("packages", { required: true })
+    .split(/(\s+)/)
+    .filter((pkg) => pkg.trim().length > 0);
+
+  await pipxInstallAction(...pkgs);
+}
+
+main().catch((err) => core.setFailed(err));
