d6aed0b5d4d8fd02af6ec4f45c5bd543b9a3451c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-15 11:21:06 +0700

    refactor: remove `main` function and move its content to top level

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import * as core from "@actions/core";
 import { pipxInstallAction } from "pipx-install-action";
 
-async function main() {
+try {
   const pkgs = core
     .getInput("packages", { required: true })
     .split(/(\s+)/)
     .filter((pkg) => pkg.trim().length > 0);
 
   await pipxInstallAction(...pkgs);
+} catch (err) {
+  core.setFailed(err);
 }
-
-main().catch((err) => core.setFailed(err));
