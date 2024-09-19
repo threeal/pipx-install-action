23786c93b1e57ae93d8638cc136436cb97ec0024
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-19 20:54:43 +0700

    test: test action entrypoint script (#317)
    * test: add test for action entrypoint script
    
    * feat: set exit code instead in action entrypoint script

---
+++ src/main.test.ts
@@ -0,0 +1,55 @@
+import { jest } from "@jest/globals";
+
+describe("install Python packages", () => {
+  let installedPackages: string[] = [];
+  jest.unstable_mockModule("./lib.js", () => ({
+    pipxInstallAction: (...pkgs: string[]): void => {
+      for (const pkg of pkgs) {
+        if (pkg === "invalid") throw new Error("invalid package");
+        installedPackages.push(pkg);
+      }
+    },
+  }));
+
+  let inputs: Record<string, string | undefined> = {};
+  let loggedError: unknown;
+  jest.unstable_mockModule("gha-utils", () => ({
+    getInput: (name: string): string => {
+      return inputs[name] ?? "";
+    },
+    logError: (err: unknown) => {
+      loggedError = err;
+    },
+  }));
+
+  beforeEach(() => {
+    jest.resetModules();
+
+    installedPackages = [];
+    inputs = {};
+    loggedError = undefined;
+    process.exitCode = undefined;
+  });
+
+  it("should install packages", async () => {
+    inputs["packages"] = "a-package another-package";
+
+    await import("../src/main.js");
+
+    expect(installedPackages).toEqual(["a-package", "another-package"]);
+    expect(loggedError).toBeUndefined();
+    expect(process.exitCode).toBeUndefined();
+  });
+
+  it("should fail to install an invalid package", async () => {
+    inputs["packages"] = "invalid";
+
+    await import("./main.js");
+
+    expect(installedPackages).toEqual([]);
+    expect(loggedError).toEqual(new Error("invalid package"));
+    expect(process.exitCode).toBe(1);
+
+    process.exitCode = undefined;
+  });
+});
