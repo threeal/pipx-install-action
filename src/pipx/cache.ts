7ec38159b5352355407eb50e517054ad828ae4be
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-24 18:55:26 +0700

    feat: modify cache name to contains package version (#332)
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/pipx/cache.ts
+++ src/pipx/cache.ts
@@ -4,14 +4,17 @@ import { addPipxPackagePath, getPipxEnvironment } from "./environment.js";
 import path from "path";
 import { parsePipxPackage } from "./utils.js";
 
+export const pipxPackageCacheVersion = "pipx-install-action-2.0.0";
+
 export async function savePipxPackageCache(pkg: string): Promise<void> {
   try {
     const localVenvs = await getPipxEnvironment("PIPX_LOCAL_VENVS");
-    const { name, version } = parsePipxPackage(pkg);
-
-    await saveCache(`pipx-${process.platform}-${name}`, version, [
-      path.join(localVenvs, name),
-    ]);
+    const { name } = parsePipxPackage(pkg);
+    await saveCache(
+      `pipx-${process.platform}-${pkg}`,
+      pipxPackageCacheVersion,
+      [path.join(localVenvs, name)],
+    );
   } catch (err) {
     throw new Error(`Failed to save ${pkg} cache: ${getErrorMessage(err)}`);
   }
@@ -19,10 +22,9 @@ export async function savePipxPackageCache(pkg: string): Promise<void> {
 
 export async function restorePipxPackageCache(pkg: string): Promise<boolean> {
   try {
-    const { name, version } = parsePipxPackage(pkg);
     const restored = await restoreCache(
-      `pipx-${process.platform}-${name}`,
-      version,
+      `pipx-${process.platform}-${pkg}`,
+      pipxPackageCacheVersion,
     );
     if (restored) await addPipxPackagePath(pkg);
     return restored;
